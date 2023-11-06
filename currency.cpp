86fa8dc3506f797ada0c4cde2ce6124895336701
InSaNiTy2442 <richcashbro1@gmail.com>
2023-11-06 00:40:16 -0600

    updated currency conversions and corrected Colombian Pesos

--- currency.cpp
+++ currency.cpp
@@ -10,9 +10,9 @@ int main() {
     std::cin >> reais;
     std::cout << "Enter number of Peruvian Soles: ";
     std::cin >> soles;
-    // Pesos conversion rate: 0.058
+    // Pesos conversion rate: 0.00025
     // Reais conversion rate: 0.20
     // Soles conversion rate: 0.27
-    dollars = 0.058 * pesos + 0.20 * reais + 0.27 * soles;
+    dollars = 0.00025 * pesos + 0.20 * reais + 0.27 * soles;
     std::cout << "Total USD = $" << dollars << "\n";
 }
